9a5687d7ecfb9db8784b563bd36392607073e9a2
dabidgmz <david.gmzherrera28@gmail.com>
2023-10-16 18:35:10 -0600

    Agregar archivos de proyecto.

---
+++ semaforolibreria/Class1.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace semaforolibreria
+{
+    public class Class1
+    {
+        private Thread thread;
+        private Luz luzVerde;
+        private Luz luzAmarilla;
+        private Luz luzRoja;
+
+        public string Nombre { get; set; }
+        public string EstadoActual { get; private set; }
+
+        public Class1(string nombre)
+        {
+            Nombre = nombre;
+            luzVerde = new Luz("Luz Verde", 11);
+            luzAmarilla = new Luz("Luz Amarilla", 12);
+            luzRoja = new Luz("Luz Roja", 13);
+        }
+
+        public void Iniciar()
+        {
+            thread = new Thread(() =>
+            {
+                while (true)
+                {
+                    EstadoActual = "Verde";
+                    luzVerde.Parpadear(3, 500);
+                    EstadoActual = "Amarillo";
+                    luzAmarilla.Encender(1000);   
+                    EstadoActual = "Rojo";
+                    luzRoja.Encender(1000);       
+                }
+            });
+
+            thread.Start();
+        }
+
+        public void Detener()
+        {
+            thread?.Abort();
+        }
+    }
+}
---
+++ semaforolibreria/Class2.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+namespace semaforolibreria
+{
+    internal class Luz
+    {
+        private string nombre;
+        private int pin;
+
+        public Luz(string nombre, int pin)
+        {
+            this.nombre = nombre;
+            this.pin = pin;
+        }
+
+        public void Encender(int duracion)
+        {
+            Console.WriteLine($"{nombre} encendida");
+
+            Thread.Sleep(duracion);
+
+            Console.WriteLine($"{nombre} apagada");
+        }
+
+        public void Parpadear(int veces, int intervalo)
+        {
+            for (int i = 0; i < veces; i++)
+            {
+                Encender(intervalo);
+                Thread.Sleep(intervalo);
+            }
+        }
+    }
+}
